b2c2e463143f46b140e2098c9ed58d3bce3801cf
Sami <96743676+sa00sami@users.noreply.github.com>
2024-04-03 06:36:09 +0300

    Add files via upload

---
+++ sami/script.js
@@ -0,0 +1,41 @@
+
+// share btn
+
+const shareBtn = document.getElementById('shareBtn')
+
+shareBtn.addEventListener('click', event => {
+
+  // Check for Web Share api support
+  if (navigator.share) {
+    // Browser supports native share api
+    navigator.share({
+      text: 'Please read this great article: ',
+      url: 'https://snaptab0.github.io/'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    // Fallback
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+
+
+// QR Code btn
+
+const section = document.querySelector(".qrcode-btn"),
+overlay = document.querySelector(".overlay"),
+showBtn = document.querySelector(".show-modal"),
+closeBtn = document.querySelector(".close-btn");
+
+showBtn.addEventListener("click", () => section.classList.add("active"));
+
+overlay.addEventListener("click", () =>
+section.classList.remove("active")
+);
+
+closeBtn.addEventListener("click", () =>
+section.classList.remove("active")
+);
+
